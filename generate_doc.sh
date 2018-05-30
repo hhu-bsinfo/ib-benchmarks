eda36a4c63a7393d3b67a7fc7fb25269f8e51d31
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2018-05-30 12:21:30 +0200

    JSocketBench: Setup project

--- generate_doc.sh
+++ generate_doc.sh
@@ -2,6 +2,10 @@
 
 rm -rf doc/
 mkdir -p doc/CVerbsBench/
+mkdir -p doc/JSocketBench/
 
 doxygen src/CVerbsBench/doxygen.conf
+doxygen src/JSocketBench/doxygen.conf
+
 ln -s CVerbsBench/html/index.html doc/CVerbsBench.html
+ln -s JSocketBench/html/index.html doc/JSocketBench.html
