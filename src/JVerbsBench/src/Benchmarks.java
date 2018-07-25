962bdb63f111f3a4787891d5a2b15095c755db23
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2018-07-25 11:28:06 +0200

    JVerbsBench: Implement message benchmark

---
+++ src/JVerbsBench/src/Benchmarks.java
@@ -0,0 +1,352 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ * Contains the benchmarks.
+ */
+class Benchmarks {
+
+    /**
+     * Send time in nanoseconds.
+     */
+    private long sendTime = 0;
+
+    /**
+     * Receive time in nanoseconds.
+     */
+    private long recvTime = 0;
+
+    /**
+     * Start the send benchmark.
+     *
+     * The measured time in nanoseconds is stored in sendTime.
+     *
+     * @param connection The connection to use for the benchmark
+     * @param msgCount The amount of message to send
+     */
+    void messageSendBenchmark(Connection connection, long msgCount) {
+        long startTime = 0;
+        long endTime = 0;
+
+        int queueSize = connection.getQueueSize();
+        int pendingComps = 0;
+
+        Log.INFO("SEND THREAD", "Starting send thread! Sending %d messages.", msgCount);
+
+        try {
+            startTime = System.nanoTime();
+
+            while(msgCount > 0) {
+                // Always post as much work requests as there are free places in the queue
+                int batchSize = queueSize - pendingComps;
+
+                if(batchSize > msgCount) {
+                    batchSize = (int) msgCount;
+                }
+
+                connection.sendMessages(batchSize);
+
+                pendingComps += batchSize;
+                msgCount -= batchSize;
+
+                // Poll only a single time
+                // It is not recommended to poll the completion queue empty, as this mostly costs too much time,
+                // which would better be spent posting new work requests
+                pendingComps -= connection.pollCompletionQueue();
+            }
+
+            // At the end, poll the completion queue until it is empty
+            while(pendingComps > 0) {
+                pendingComps -= connection.pollCompletionQueue();
+            }
+
+            endTime = System.nanoTime();
+        } catch(Exception e) {
+            Log.ERROR_AND_EXIT("SEND THREAD", "An error occurred, while sending a message!" +
+                    " Error: '%s'", e.getMessage());
+        }
+
+        Log.INFO("SEND THREAD", "Finished sending!");
+
+        sendTime = endTime - startTime;
+
+        Log.INFO("SEND THREAD", "Terminating thread...");
+    }
+
+    /**
+     * Start the receive benchmark.
+     *
+     * The measured time in nanoseconds is stored in recvTime.
+     *
+     * @param connection The connection to use for the benchmark
+     * @param msgCount The amount of message to send
+     */
+    void messageRecvBenchmark(Connection connection, long msgCount) {
+        long startTime = 0, endTime = 0;
+
+        int queueSize = connection.getQueueSize();
+        int pendingComps = 0;
+
+        Log.INFO("RECV THREAD", "Starting receive thread! Receiving %d messages.", msgCount);
+
+        try {
+            startTime = System.nanoTime();
+
+            while(msgCount > 0) {
+                // Always post as much work requests as there are free places in the queue
+                int batchSize = queueSize - pendingComps;
+
+                if(batchSize > msgCount) {
+                    batchSize = (int) msgCount;
+                }
+
+                connection.recvMessages(batchSize);
+
+                pendingComps += batchSize;
+                msgCount -= batchSize;
+
+                // Poll only a single time
+                // It is not recommended to poll the completion queue empty, as this mostly costs too much time,
+                // which would better be spent posting new work requests
+                pendingComps -= connection.pollCompletionQueue();
+            }
+
+            // At the end, poll the completion queue until it is empty
+            while(pendingComps > 0) {
+                pendingComps -= connection.pollCompletionQueue();
+            }
+
+            endTime = System.nanoTime();
+        } catch (Exception e) {
+            Log.ERROR_AND_EXIT("RECV THREAD", "An error occured, while receiving a message!" +
+                    " Error: '%s'", e.getMessage());
+        }
+
+        Log.INFO("RECV THREAD", "Finished sending!");
+
+        recvTime = endTime - startTime;
+
+        Log.INFO("RECV THREAD", "Terminating thread...");
+    }
+
+    /**
+     * Start the rdma write benchmark.
+     *
+     * The measured time in nanoseconds is stored in sendTime.
+     *
+     * @param connection The connection to use for the benchmark
+     * @param count The amount of writes to perform
+     */
+    void rdmaSendBenchmark(Connection connection, long count) {
+        long startTime = 0;
+        long endTime = 0;
+
+        int queueSize = connection.getQueueSize();
+        int pendingComps = 0;
+
+        Log.INFO("SEND THREAD", "Starting send thread! Sending %d messages.", count);
+
+        try {
+            startTime = System.nanoTime();
+
+            while(count > 0) {
+                // Always post as much work requests as there are free places in the queue
+                int batchSize = queueSize - pendingComps;
+
+                if(batchSize > count) {
+                    batchSize = (int) count;
+                }
+
+                connection.rdmaWrite(batchSize);
+
+                pendingComps += batchSize;
+                count -= batchSize;
+
+                // Poll only a single time
+                // It is not recommended to poll the completion queue empty, as this mostly costs too much time,
+                // which would better be spent posting new work requests
+                pendingComps -= connection.pollCompletionQueue();
+            }
+
+            // At the end, poll the completion queue until it is empty
+            while(pendingComps > 0) {
+                pendingComps -= connection.pollCompletionQueue();
+            }
+
+            endTime = System.nanoTime();
+        } catch(Exception e) {
+            Log.ERROR_AND_EXIT("SEND THREAD", "An error occurred, while sending a message!" +
+                    " Error: '%s'", e.getMessage());
+        }
+
+        Log.INFO("SEND THREAD", "Finished sending!");
+
+        sendTime = endTime - startTime;
+
+        Log.INFO("SEND THREAD", "Sending 'close'-command to remote host.");
+
+        try {
+            DataOutputStream outStream = new DataOutputStream(connection.getSocket().getOutputStream());
+
+            outStream.write("close".getBytes());
+        } catch (Exception e) {
+            Log.ERROR_AND_EXIT("SEND THREAD", "An error occurred, while sending 'close'! Error: '%s'",
+                    e.getMessage());
+        }
+
+        Log.INFO("SEND THREAD", "Terminating thread...");
+    }
+
+    /**
+     * Start the rdma receive benchmark.
+     *
+     * The measured time in nanoseconds is stored in sendTime.
+     *
+     * @param connection The connection to use for the benchmark
+     */
+    void rdmaRecvBenchmark(Connection connection) {
+        long startTime = 0;
+        long endTime = 0;
+
+        byte[] buf = new byte[5];
+
+        Log.INFO("RECV THREAD", "Starting receive thread!");
+
+        startTime = System.nanoTime();
+
+        try {
+            DataInputStream inStream = new DataInputStream(connection.getSocket().getInputStream());
+
+            inStream.readFully(buf);
+        } catch (Exception e) {
+            Log.ERROR_AND_EXIT("RECV THREAD", "An error occurred, while receiving 'close'! Error: '%s'",
+                    e.getMessage());
+        }
+
+        endTime = System.nanoTime();
+
+        Log.INFO("RECV THREAD", "Received 'close'-command from remote host!");
+
+        Log.INFO("SEND THREAD", "Finished receiving!");
+
+        recvTime = endTime - startTime;
+
+        Log.INFO("RECV THREAD", "Terminating thread...");
+    }
+
+    /**
+     * Start the pingpong benchmark as server
+     *
+     * The measured time in nanoseconds is stored in sendTime.
+     *
+     * @param connection The connection to use for the benchmarks
+     * @param msgCount The amount of message to send and receive
+     */
+    void pingPongBenchmarkServer(Connection connection, long msgCount) {
+        long startTime = 0;
+        long endTime = 0;
+
+        int polled;
+
+        Log.INFO("SERVER THREAD", "Starting pingpong server thread!");
+
+        try {
+            startTime = System.nanoTime();
+
+            while(msgCount > 0) {
+                // Send a single message and wait until a work completion is generated
+                connection.sendMessages(1);
+
+                do {
+                    polled = connection.pollCompletionQueue();
+                } while(polled == 0);
+
+                // Receive a single message and wait until a work completion is generated
+                connection.recvMessages(1);
+
+                do {
+                    polled = connection.pollCompletionQueue();
+                } while(polled == 0);
+
+                msgCount--;
+            }
+
+            endTime = System.nanoTime();
+        } catch(Exception e) {
+            Log.ERROR_AND_EXIT("SERVER THREAD", "An error occurred, while sending or receiving a message!" +
+                    " Error: '%s'", e.getMessage());
+        }
+
+        Log.INFO("SERVER THREAD", "Finished pingpong test!");
+
+        sendTime = endTime - startTime;
+
+        Log.INFO("SERVER THREAD", "Terminating thread...");
+    }
+
+    /**
+     * Start the pingpong benchmark as client
+     *
+     * The measured time in nanoseconds is stored in sendTime.
+     *
+     * @param connection The connection to use for the benchmarks
+     * @param msgCount The amount of message to send and receive
+     */
+    void pingPongBenchmarkClient(Connection connection, long msgCount) {
+        long startTime = 0;
+        long endTime = 0;
+
+        int polled;
+
+        Log.INFO("CLIENT THREAD", "Starting pingpong client thread!");
+
+        try {
+            startTime = System.nanoTime();
+
+            while(msgCount > 0) {
+                // Receive a single message and wait until a work completion is generated
+                connection.recvMessages(1);
+
+                do {
+                    polled = connection.pollCompletionQueue();
+                } while(polled == 0);
+
+                // Send a single message and wait until a work completion is generated
+                connection.sendMessages(1);
+
+                do {
+                    polled = connection.pollCompletionQueue();
+                } while(polled == 0);
+
+                msgCount--;
+            }
+
+            endTime = System.nanoTime();
+        } catch(Exception e) {
+            Log.ERROR_AND_EXIT("CLIENT THREAD", "An error occurred, while sending or receiving a message!" +
+                    " Error: '%s'", e.getMessage());
+        }
+
+        Log.INFO("CLIENT THREAD", "Finished pingpong test!");
+
+        sendTime = endTime - startTime;
+
+        Log.INFO("CLIENT THREAD", "Terminating thread...");
+    }
+
+    /**
+>>>>>>> 8d5b824...  s jvbench
+     * Get the measured send time.
+     */
+    long getSendTime() {
+        return sendTime;
+    }
+
+    /**
+     * Get the measured receive time.
+     */
+    long getRecvTime() {
+        return recvTime;
+    }
+}
+
