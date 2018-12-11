e5e58f0044282a9c6ea588e29a1bb52e6b3107cb
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-12-11 13:34:27 +0100

    hilbert: Adapt to allow different log output folder

--- hilbert.sh
+++ hilbert.sh
@@ -6,17 +6,20 @@ readonly NODES=($( < pbs_nodes ))
 readonly SERVER="10.100.0.$(echo "${NODES[0]}" | sed -e "s/.hilbert.hpc.uni-duesseldorf.de//" | cut -c 8-)"
 readonly CLIENT="10.100.0.$(echo "${NODES[1]}" | sed -e "s/.hilbert.hpc.uni-duesseldorf.de//" | cut -c 8-)"
 
-readonly BENCHMARK_PATH="${1}"
-readonly JAVA_PATH="${2}"
-readonly J9_JAVA_PATH="${3}"
-readonly LIBVMA_PATH="${4}"
+readonly RESULTS_OUTPATH="${1}"
+readonly BENCHMARK_PATH="${2}"
+readonly JAVA_PATH="${3}"
+readonly J9_JAVA_PATH="${4}"
+readonly LIBVMA_PATH="${5}"
 
 cd "${BENCHMARK_PATH}" || exit 1
 
 if [ "${PBS_NODENUM}" -eq "0" ]; then
-    ./run_all.sh -m "server" -a "${SERVER}" -rs compat -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" > ${SCRIPT_DIR}/server.log 2>&1
+    echo "dummy" > "${RESULTS_OUTPATH}/server.log"
+    ./run_all.sh -m "server" -a "${SERVER}" -rs compat -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" -o "${RESULTS_OUTPATH}" > "${RESULTS_OUTPATH}/server.log" 2>&1
 elif [ "${PBS_NODENUM}" -eq "1" ]; then
-    sleep 10 && ./run_all.sh -m client -r "${SERVER}" -a "${CLIENT}" -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" > ${SCRIPT_DIR}/client.log 2>&1
+    echo "dummy" > "${RESULTS_OUTPATH}/client.log"
+    sleep 10 && ./run_all.sh -m client -r "${SERVER}" -a "${CLIENT}" -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" -o "${RESULTS_OUTPATH}" > "${RESULTS_OUTPATH}/client.log" 2>&1
 fi
 
 cd ~ || exit 1
