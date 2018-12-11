8d9faa4198f7e5a56c43540da4fb9ca49a249397
Stefan Nothaas <stefan.nothaas@hhu.de>
2018-12-11 13:33:48 +0100

    hhubs.sh: Adapt to allow different log output folder

--- hhubs.sh
+++ hhubs.sh
@@ -9,6 +9,11 @@ cd $SCRIPT_DIR
 source ./settings.conf
 
 if [ "${1}" = "server" ]; then
+    rm -r $RESULTS_OUTPATH
+    mkdir -p $RESULTS_OUTPATH
+fi
+
+if [ "${1}" = "server" ]; then
     echo "Building..."
     ./build_all.sh -m build -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}"
 
@@ -18,10 +23,10 @@ if [ "${1}" = "server" ]; then
     fi
 
     echo "Running as server"
-    ./run_all.sh -m "server" -a "${SERVER}" -rs compat -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" # > ./server.log 2>&1
+    ./run_all.sh -m "server" -a "${SERVER}" -rs compat -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" -o "${RESULTS_OUTPATH}" > "${RESULTS_OUTPATH}/server.log" 2>&1
 elif [ "${1}" = "client" ]; then
     echo "Running as client"
-    sleep 10 && ./run_all.sh -m client -r "${SERVER}" -a "${CLIENT}" -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" #> ./client.log 2>&1
+    sleep 10 && ./run_all.sh -m client -r "${SERVER}" -a "${CLIENT}" -i "${J9_JAVA_PATH}" -j "${JAVA_PATH}" -l "${LIBVMA_PATH}" -o "${RESULTS_OUTPATH}" > "${RESULTS_OUTPATH}/client.log" 2>&1
 else
     echo "Usage: ${0} <type server/client>"
     exit 1
